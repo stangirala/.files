278a95dafddc7cf5b2655e1cc8530a4a866f4d31
Sarma Tangirala <tvssarma.omega9@gmail.com>
2018-04-23 11:42:02 -0700

    stuff

--- vim_plugin_setup.sh
+++ vim_plugin_setup.sh
@@ -3,3 +3,4 @@ BASE=$HOME/.vim/bundle/
 git clone https://github.com/kien/ctrlp.vim.git $BASE/ctrlp.vim
 git clone https://github.com/scrooloose/nerdtree.git $BASE/nerdtree
 git clone https://github.com/easymotion/vim-easymotion $BASE/vim-easymotion
+git clone --recursive https://github.com/davidhalter/jedi-vim.git $BASE/jedi-vim
