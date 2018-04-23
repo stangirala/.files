36cd46d8d73e83ef1870691adb353b9d015f8207
Sarma Tangirala <tvssarma.omega9@gmail.com>
2018-04-22 18:32:20 -0700

    plugin setup

---
+++ vim_plugin_setup.sh
@@ -0,0 +1,5 @@
+BASE=$HOME/.vim/bundle/
+
+git clone https://github.com/kien/ctrlp.vim.git $BASE/ctrlp.vim
+git clone https://github.com/scrooloose/nerdtree.git $BASE/nerdtree
+git clone https://github.com/easymotion/vim-easymotion $BASE/vim-easymotion
